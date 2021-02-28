5b86093176c3411c90d77db16e6dd9317d072b31
Bdfy <aiven1987@mail.ru>
2021-02-28 22:10:44 +0500

    add vetka1_2

---
+++ primer1_vetka1/src/main/java/ru/myuniquenickname/primer1_vetka1/Primer1_vetka1_2.kt
@@ -0,0 +1,11 @@
+package ru.myuniquenickname.primer1_vetka1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Primer1_vetka1_2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_primer1_vetka1)
+    }
+}
